ffb67bd322a90b6605fed16759426fb3c41b8ce6
Gogo Lejzr <ggljzr@localhost.localdomain>
2015-09-16 05:15:59 +0200

    now also reads local sensor on raspberry pi (using pango markup, update themes)

--- weatherBar.py
+++ weatherBar.py
@@ -1,25 +1,68 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+#gets data from openweathermap and from local raspberry pi with dht11 sensor
+#requires pango markup in weather widget
 import sys
 
 import urllib2
 import json
 
+piFlaskPort = '192.168.0.105:5000'
 
+response = None
+localResponse = None
+
+localDataColor = '"#C36661"'
+markup = 'foreground = ' + localDataColor
+
+temp = '-'
+tempLocal = '<span '+ markup + ' >-</span>'
+hum =  '-'
+humLocal = '<span '+ markup + ' >-</span>'
+
+
+#get openweathermap data
 try:
 	response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=Prague&units=metric')
 except urllib2.URLError:
-	sys.stdout.write('No internet connection')
+	pass
 else:
 	rawData = response.read()
 	jsonData = json.loads(rawData)
-
-
+	
 	try:
 		main = jsonData['main']
 	except KeyError:
-		sys.stdout.write('No weather data')
+		pass
 	else:
-		temp = 'Temperature ' + str(main['temp']) + u' °C'
-		hum = ' Humidity ' + str(main['humidity']) + ' %'
-		sys.stdout.write(temp.encode('utf-8') + ' /' + hum)
+		temp = str(main['temp'])
+		hum = str(main['humidity'])
+
+#get local data
+try:
+	localResponse = urllib2.urlopen('http://' + piFlaskPort + '/getTemp' )
+except urllib2.URLError:
+	pass	
+
+else:
+	rawLocalData = localResponse.read()
+	jsonLocalData = json.loads(rawLocalData)
+
+	try:
+		tempLocal = '<span '+ markup + ' >' + str(jsonLocalData['temp']) + '</span>'
+		humLocal = '<span '+ markup + ' >' + str(jsonLocalData['hum']) + '</span>'
+	except KeyError:
+		pass
+
+
+
+if response is None and localResponse is None:
+	sys.stdout.write('No connection')
+	sys.exit()
+
+sys.stdout.write('Temperature ' + temp + ' (' + tempLocal + ') C / Humidity ' + hum + 
+		 ' (' + humLocal + ') %')
+
+
+
