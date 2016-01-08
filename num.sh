4a7ff28c010928b81e0dfbaa730ade5e43d1cce0
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-01-08 02:19:44 +0100

    script for numlock status

---
+++ num.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+LED=$(xset q | grep LED | cut -d: -f4 | tr -d " ")
+
+OFF_COLOR=$(cat /home/ggljzr/.Xresources | grep "color8" | cut -d: -f2 | tr -d " ")
+ON_COLOR=$(cat /home/ggljzr/.Xresources | grep "color14" | cut -d: -f2 | tr -d " ")
+
+if (( ($LED & 2) >> 1 )) 
+then
+	echo '<span foreground = "'$ON_COLOR'">NUM</span>'
+        #echo "NUM ON"	
+else
+	echo '<span foreground = "'$OFF_COLOR'">NUM</span>'
+	#echo "NUM OFF"
+fi
+
