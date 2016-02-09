9f9d8721e8d26d19c15e29b4a92ceaf1a3235a65
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-02-09 22:56:56 +0100

    added font support

--- themes/simple_themes/themeScript2.sh
+++ themes/simple_themes/themeScript2.sh
@@ -5,43 +5,41 @@ ff_profile="az8dnp0b.default-1441248752478"
 declare -a templates
 IFS=' ' read -r -a templates <<< $(ls $template_path)
 
+font_size=$(cat $1 | grep font | cut -d: -f3)
+font_family=$(cat $1 | grep font | cut -d: -f2)
+
+bckg=$(cat $1 | grep bckg | cut -d: -f2)
+fore=$(cat $1 | grep fore | cut -d: -f2)
+
 for entry in "${templates[@]}"
 do
 
 
-	bckg=$(cat $1 | grep bckg | cut -d: -f2)
 	sed s/"##bckg##"/$bckg/ $template_path/$entry > "$entry".temp
-
-	fore=$(cat $1 | grep fore | cut -d: -f2)
-	sed s/"##fore##"/$fore/ "$entry".temp > temp
-
-	mv temp "$entry".temp
+	sed -i s/"##fore##"/$fore/ "$entry".temp
+	
+	sed -i s/"##font_size##"/$font_size/ "$entry".temp
+	sed -i s/"##font_family##"/$font_family/ "$entry".temp
 
 	for i in {0..15}
 	do
 		color=$(cat $1 | grep "color$i:" | cut -d: -f2)
-		sed s/"##color$i##"/$color/ "$entry".temp > temp
-		mv temp "$entry".temp	
+		sed -i s/"##color$i##"/$color/ "$entry".temp
 	done
-
 done
 
 border=$(cat $1 | grep border | cut -d: -f2)
-sed s/"##border##"/$border/ config.temp> temp
-mv temp config.temp
+sed -i s/"##border##"/$border/ config.temp
 
 #parsovani barvy do firefox podle toho jesli je theme dark
 #nebo light
 ff_theme=$(cat $1 | grep ff_theme | cut -d: -f2)
 ff_theme_color=$(cat $1 | grep "$ff_theme"Grey | cut -d: -f2)
-sed s/"##special1##"/$ff_theme_color/ newtab.css.temp > temp
-mv temp newtab.css.temp
+sed -i s/"##special1##"/$ff_theme_color/ newtab.css.temp
 
-sed s/"##special1##"/$ff_theme_color/ userChrome.css.temp > temp
-mv temp userChrome.css.temp
+sed -i s/"##special1##"/$ff_theme_color/ userChrome.css.temp
 
-sed s/"##special1##"/$ff_theme_color/ redmond.vimp.temp > temp
-mv temp redmond.vimp.temp
+sed -i s/"##special1##"/$ff_theme_color/ redmond.vimp.temp
 
 mv xres.temp ~/.Xresources
 mv config.temp ~/.i3/config
