e8b37dbac8b2463c951bd25e99182b5c1ece5349
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-07-12 01:29:11 +0200

    added shitty script to get current spotify song

---
+++ current_song.py
@@ -0,0 +1,14 @@
+import sys
+import subprocess as sb
+
+try:
+    out = sb.check_output("xwininfo -tree -root | grep Spotify | head -n 1 | cut -d: -f1", shell=True)
+except:
+    out = "Some error"
+
+out = str(out)
+out = out.split('"')[1]
+
+if out != "Spotify":
+    sys.stdout.write(out)
+
